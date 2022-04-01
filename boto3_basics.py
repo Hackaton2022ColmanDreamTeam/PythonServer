a16f352bf917d31ffe7e26df534be0a18b239076
shaharkozi <102664929+shaharkozi@users.noreply.github.com>
2022-04-01 18:05:25 +0300

    ec2-connect

---
+++ boto3_basics.py
@@ -0,0 +1,24 @@
+
+import boto3
+from credentials import AWS_ACCESS_KEY, AWS_SECRET_KEY
+
+INSTANCE_ID = "i-0b5400a300b56a676"
+
+session = boto3.Session(
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY,
+)
+
+ec2 = session.resource("ec2")
+
+for instance in ec2.instances.all():
+    if instance.id == INSTANCE_ID:
+        print(
+            f"""ID: {instance.id}
+            \nType: {instance.instance_type}
+            \nPublic IPv4: {instance.public_ip_address}
+            \nAMI: {instance.image.id}
+            \nState: {instance.state}
+            \n
+            """
+        )
