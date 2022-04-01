b8f5a9766ac9212abe2796dbf08b5bb2288ae47e
OrBenNun <obn2468@gmail.com>
2022-04-01 18:46:21 +0300

    fix

--- boto3_basics.py
+++ boto3_basics.py
@@ -2,11 +2,12 @@
 import boto3
 from credentials import AWS_ACCESS_KEY, AWS_SECRET_KEY
 
-INSTANCE_ID = "i-0b5400a300b56a676"
+INSTANCE_ID = "i-075b0fe0809290316"
 
 session = boto3.Session(
     aws_access_key_id=AWS_ACCESS_KEY,
     aws_secret_access_key=AWS_SECRET_KEY,
+    region_name='us-east-1'
 )
 
 ec2 = session.resource("ec2")
